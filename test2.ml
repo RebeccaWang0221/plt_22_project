70f8a1e0fe028ac2c3d5f1bbf89f61b252643d91
Matthew <mt3131@columbia.edu>
2020-05-04 14:01:27 -0700

    moved to test folder

--- test2.ml
+++
@@ -1,9 +0,0 @@
-open Sast
-
-module StringMap = Map.Make(String)
-
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
-  let sprogram = Semant.check program StringMap.empty StringMap.empty in
-  print_endline "Done"
