9c58ea1d5696e8743cd8af27af9736201aa193b0
Matthew <mt3131@columbia.edu>
2020-05-08 13:44:50 -0700

    c integer list

---
+++ intlist.h
@@ -0,0 +1,29 @@
+struct IntNode {
+    int val;
+    struct IntNode *next;
+};
+
+struct IntList {
+    struct IntNode *head;
+    int size;
+};
+
+struct IntNode* create_int_node(int val, struct IntNode *next);
+
+void init_int_list(struct IntList *lst);
+
+void append_int(struct IntList *lst, int val);
+
+int get_int(struct IntList *lst, int idx);
+
+void remove_int(struct IntList *lst, int idx);
+
+void print_int_list(struct IntList *lst);
+
+void insert_int(struct IntList *lst, int idx, int val);
+
+int index_int(struct IntList *lst, int val);
+
+int pop_int(struct IntList *lst, int idx);
+
+void free_int_list(struct IntList *lst);
