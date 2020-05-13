5f6d044e017fb1b69adc614dd1ecfa8824be4265
Matthew <mt3131@columbia.edu>
2020-05-13 11:47:24 -0700

    fixed if error inside for loop

--- strops.h
+++ strops.h
@@ -1,6 +1,6 @@
-int str_comp(char *s1, char *s2);
+char str_comp(char *s1, char *s2);
 
-int str_diff(char *s1, char *s2);
+char str_diff(char *s1, char *s2);
 
 char *str_concat(char *s1, char *s2);
 
